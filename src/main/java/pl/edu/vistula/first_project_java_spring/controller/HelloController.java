9f19f0b5b20a3a938276aa4b2cb5de56ce0b2bde
Emmanuel <emmanuelfidelis.ken@gmail.com>
2025-05-22 20:09:50 +0100

    final commit

--- src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_project_java_spring/controller/HelloController.java
@@ -5,19 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//@Controller
-@RestController
+
+//@RestController
+
+@Controller
 public class HelloController {
 
-    @GetMapping(value = "/")
+    @GetMapping( "/")
     public String hello() {
-        return "Hello ken, in my first Spring controller.";
+        return "Hello Vistula, in my first Spring controller.";
     }
 
     @GetMapping( "/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue = "World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-    //https://localhost:8080/greeting?name=ken
+    //https://localhost:8080/greeting?name=Ken
 }
